ac1001311c01f16d785ddbead83319b58fa59eee
Muart-C <charles.muathe@strathmore.edu>
2018-08-25 11:36:07 +0300

    Updated the CPI endpoints

--- routes/web.php
+++ routes/web.php
@@ -28,7 +28,14 @@ Route::get('Agriculture/gross_production', 'Agriculture@agriculture_gross_market
 
 // @Charles Ndirangu
 // CPI get cpi_annual_avg_retail_prices_of_certain_consumer_goods_in_kenya route
-
-
 Route::get('CPI/cpi_annual_avg_retail_prices_of_certain_consumer_goods_in_kenya', 'Endpoints\CPI@get_cpi_annual_avg_retail_prices_of_certain_consumer_goods_in_kenya')->name('cpi_annual_avg_retail_prices_of_certain_consumer_goods_in_kenya');
+
+// CPI get cpi_consumer_price_index route
+Route::get('CPI/cpi_consumer_price_index', 'Endpoints\CPI@get_cpi_consumer_price_index')->name('cpi_consumer_price_index');
+
+// CPI get cpi_elementary_aggregates_weights_in_the_cpi_baskets route
+Route::get('CPI/cpi_elementary_aggregates_weights_in_the_cpi_baskets', 'Endpoints\CPI@get_cpi_elementary_aggregates_weights_in_the_cpi_baskets')->name('cpi_elementary_aggregates_weights_in_the_cpi_baskets');
+
+// CPI get cpi_group_weights_for_kenya_cpi_october_base_1997 route
+Route::get('CPI/cpi_group_weights_for_kenya_cpi_october_base_1997', 'Endpoints\CPI@get_cpi_group_weights_for_kenya_cpi_october_base_1997')->name('cpi_group_weights_for_kenya_cpi_febuary_base_2009');
 
